396d4b4b79f40248139320f2c471e31b82e443d5
kKapalka <k.kapalka@hotmail.com>
2024-06-22 14:20:09 +0200

    mainManu + saveData

---
+++ scripts/menu/mainMenu.gd
@@ -0,0 +1,32 @@
+extends Node
+
+class_name MainMenu
+
+var activeMenu: CanvasGroup = null
+
+@onready var welcomeMenu: WelcomeScreen = $WelcomeScreen
+@onready var playMenu: CanvasGroup = $PlayScreen
+@onready var settingsMenu: CanvasGroup = $SettingsScreen
+@onready var creditsMenu: CanvasGroup = $CreditsScreen
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	switch(welcomeMenu)
+	welcomeMenu.mainMenu = self
+
+
+func switch(newMenu: CanvasGroup):
+	if (activeMenu != null):
+		activeMenu.visible = false
+	activeMenu = newMenu
+	activeMenu.visible = true
+
+func switchByName(menuName: String):
+	match menuName:
+		"welcome":
+			switch(welcomeMenu)
+		"play":
+			switch(playMenu)
+		"settings":
+			switch(settingsMenu)
+		"credits":
+			switch(creditsMenu)
